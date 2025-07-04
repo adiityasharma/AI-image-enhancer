6b324bcfd2efb966cb7565b99df45e4908724413
Aditya Sharma <adityasharma626367@gmail.com>
2025-07-04 16:31:29 +0530

    Enhance ImageUpload component with drag-and-drop functionality and improved file handling

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ImageUpload(props) {
+
+  
+  const [dragging, setDragging] = useState(false)
+
+  const handleDrop = (e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    setDragging(false)
+
+    const droppedFile = e.dataTransfer.files[0];
+    if(droppedFile){
+      props.uploadImageHandler(droppedFile)
+    }
+    console.log(file)
+  }
+
+  const handleDragOver = (e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    setDragging(true);
+    
+  }
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragging(false);
+  };
+
+
 
 
   const showImageHandler = (e)=>{
@@ -12,16 +42,16 @@ function ImageUpload(props) {
 
 
   return (
-    <div className="bg-white shadow-lg px-6 py-5 w-full max-w-2xl rounded-2xl">
+    <div onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave} className="bg-white shadow-lg px-6 py-5 w-full max-w-2xl rounded-2xl">
       <label
         htmlFor="selectFile"
-        className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-2xl hover:border-blue-500 transition-all"
+        className={`block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-2xl ${dragging? "border-blue-500" : ""} hover:border-blue-500 transition-all `}
       >
-        <p className="px-5 py-3 hover:text-blue-600 transition-all">
+        <p className={`px-5 py-3 ${dragging? "text-blue-600" : ""} hover:text-blue-600 transition-all`} >
           Click or drag your image here to upload
         </p>
       </label>
-      <input onChange={showImageHandler} type="file" id="selectFile" className="hidden" />
+      <input  onChange={showImageHandler} type="file" id="selectFile" className="hidden" />
     </div>
   );
 }
